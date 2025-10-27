dbe452e44b4349e831ce1ec3291e68f43943a936
Septian Adi Pratama <septianadip7998@gmail.com>
2025-10-28 00:04:40 +0700

    ssuccesfully get data

--- server.js
+++ server.js
@@ -10,8 +10,25 @@ const db = mysql.createConnection({
   password: "",
 });
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the School Database API");
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Connected to the database");
+
+    const sql = "SELECT * FROM user";
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err);
+      } else {
+        console.log("Hasil database:", results);
+      }
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Welcome to the School Database API"); 
+    });
+  }
 });
 
 app.listen(8000, () => {
