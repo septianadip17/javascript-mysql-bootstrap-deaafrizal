878e2cc9e42134c14945034ddd1000156e82d131
Septian Adi Pratama <septianadip7998@gmail.com>
2025-10-27 23:56:09 +0700

    preparation

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  database: "db_school",
+  user: "root",
+  password: "",
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the School Database API");
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
